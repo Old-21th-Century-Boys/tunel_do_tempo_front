300cd5a522d8ef5eb1568f1d8d6fb46c9898409d
João Gabriel <joaogabrielsr39@gmail.com>
2024-07-14 17:15:11 -0300

    fix: fix incorrectly vite aliases configuration

--- vite.config.ts
+++ vite.config.ts
@@ -7,14 +7,14 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './tunel_do_tempo_front/src'),
-      '@Components': path.resolve(__dirname, './tunel_do_tempo_front/src/components'),
-      '@Contexts': path.resolve(__dirname, './tunel_do_tempo_front/src/contexts'),
-      '@Pages': path.resolve(__dirname, './tunel_do_tempo_front/src/pages'),
-      '@Routes': path.resolve(__dirname, './tunel_do_tempo_front/src/routes'),
-      '@Services': path.resolve(__dirname, './tunel_do_tempo_front/src/services'),
-      '@Types': path.resolve(__dirname, './tunel_do_tempo_front/src/types'),
-      '@Utils': path.resolve(__dirname, './tunel_do_tempo_front/src/utils'),
+      '@': path.resolve(__dirname, './src'),
+      '@Components': path.resolve(__dirname, './src/components'),
+      '@Contexts': path.resolve(__dirname, './src/contexts'),
+      '@Pages': path.resolve(__dirname, './src/pages'),
+      '@Routes': path.resolve(__dirname, './src/routes'),
+      '@Services': path.resolve(__dirname, './src/services'),
+      '@Types': path.resolve(__dirname, './src/types'),
+      '@Utils': path.resolve(__dirname, './src/utils'),
     },
   },
 });
