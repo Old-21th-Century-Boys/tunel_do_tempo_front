28d963ceffde7670626e95531759350631f284d7
jopei <joaomoura.269@gmail.com>
2024-07-21 10:38:11 -0300

    feat: Creating connection route with the api

---
+++ src/components/MembrosExempleAPI.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+
+import api from '../services/axiosConfig';
+
+type Member = {
+  id: number;
+  nome: string;
+};
+
+const ExampleComponent: React.FC = () => {
+  const [data, setData] = useState<Member[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await api.get('/membros');
+        setData(response.data);
+      } catch (error) {
+        console.error('Erro ao buscar dados:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Dados da API</h1>
+      <ul>
+        {data.map((item, index) => (
+          <li key={index}>{item.nome}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ExampleComponent;
